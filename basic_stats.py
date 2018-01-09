3e1f49e919c866c384abed7177298d5a8fa0b89d
Lucas Cavalare <lucascavalare@gmail.com>
2018-01-09 17:44:06 -0200

    Creating a hybrid module

--- basic_stats.py
+++ basic_stats.py
@@ -6,12 +6,16 @@ Compute some simple descriptive statistics.
 from ch_5_ex_1 import mean, mode, median
 from ch_5_ex_1 import get_deaths, get_cheese
 
-year_deaths = list( get_deaths() )
-years = list( year for year, death in year_deaths )
-deaths= list( death for year, death in year_deaths )
-print( "Year Range", min(years), "to", max(years) )
-print( "Average Deaths {:.2f}".format( mean( deaths ) ) )
-year_cheese= get_cheese()
-
-print( "Average Cheese Consumption", 
+def analyze_cheese_deaths():
+    
+    year_deaths = list( get_deaths() )
+    years = list( year for year, death in year_deaths )
+    deaths= list( death for year, death in year_deaths )
+    print( "Year Range", min(years), "to", max(years) )
+    print( "Average Deaths {:.2f}".format( mean( deaths ) ) )
+    year_cheese= get_cheese()
+    print( "Average Cheese Consumption", 
     mean( [cheese for year, cheese in year_cheese] ) )
+  
+if __name__ == "__main__":
+    analyze_cheese_deaths()

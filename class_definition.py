497f4016e42ae9f3f5b26333728addd14a3a1c11
Lucas Cavalare <lucascavalare@gmail.com>
2018-01-10 16:13:33 -0200

    Creating class_definition

---
+++ class_definition.py
@@ -0,0 +1,11 @@
+  from ch_5_ex_1 import get_deaths, get_cheese
+deaths = AnnualStats( get_deaths() )
+cheese = AnnualStats( get_cheese() )
+
+print("Year Range", deaths.min_year(), deaths.max_year())
+print("Average W75 Deaths", deaths.mean())
+
+print("Median Cheese Consumption", cheese.median())
+print("Mean Cheese Consumption", cheese.mean())
+
+print(deaths )

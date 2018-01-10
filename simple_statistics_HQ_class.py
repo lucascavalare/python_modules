bac5f39ba78fa0efaefc11424299e8a2b3b2483e
Lucas Cavalare <lucascavalare@gmail.com>
2018-01-10 15:50:20 -0200

    Createing simple_statistics_HQ_class

---
+++ simple_statistics_HQ_class.py
@@ -0,0 +1,24 @@
+rom collections import Counter
+class AnnualStats:
+    def __init__(self, year_measure):
+        self.year_measure = list(year_measure)
+        self.data = list(v for yr, v in self.year_measure)
+        self.counter= Counter(self.data)
+    def __repr__(self):
+        return repr(self.year_measure)
+    def min_year(self):
+        return min( yr for yr, v in self.year_measure )
+    def max_year(self):
+        return max( yr for yr, v in self.year_measure )
+    def mean(self):
+        return sum(self.data)/len(self.data)
+    def median(self):
+        mid, odd = divmod( len(self.data), 2 )
+        if odd:
+            return sorted(self.data)[mid]
+        else:
+            pair= sorted(self.data)[mid-1:mid+1]
+            return sum(pair)/2
+    def mode(self):
+        value, count = self.counter.most_common1)[0]
+        return value

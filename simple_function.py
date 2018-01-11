0470cbdcb0842f1d51a5dfca03983e36af2d69cd
Lucas Cavalare <lucascavalare@gmail.com>
2018-01-11 15:49:01 -0200

    Creating simple_function file

---
+++ simple_function.py
@@ -0,0 +1,7 @@
+def mean(values):
+    """Mean of a sequence (doesn't work with an iterable)
+    >>> from ch_5_ex_1 import mean
+    >>> mean( [2, 4, 4, 4, 5, 5, 7, 9])
+    5.0
+    """
+    return sum(values)/len(values

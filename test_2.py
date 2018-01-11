39fe8b924d4b38c265108953ba05b4a708adbaac
Lucas Cavalare <lucascavalare@gmail.com>
2018-01-11 17:09:52 -0200

    Creating test_2.py

---
+++ test_2.py
@@ -0,0 +1,11 @@
+"""Chapter 5, example 1
+Some simple statistical functions.
+>>> from ch_5_ex_1 import mean, median
+>>> data = [2, 4, 4, 4, 5, 5, 7, 9]
+>>> data # doctest: +ELLIPSIS
+[2, 4..., 9]
+>>> mean( data )
+5.0
+>>> median( data )
+4.5
+"""
